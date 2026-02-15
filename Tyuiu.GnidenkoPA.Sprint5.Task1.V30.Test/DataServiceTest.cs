8040589341131bb27a2041431162d773da9951bf
gnide <gnide@DESKTOP-FA1JVTU>
2026-02-15 18:58:48 +0500

    Task1

---
+++ Tyuiu.GnidenkoPA.Sprint5.Task1.V30.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.GnidenkoPA.Sprint5.Task1.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.GnidenkoPA.Sprint5.Task1.V30/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Tyuiu.GnidenkoPA.Sprint5.Task1.V30.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint5.Task1.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startValue = -5;
+            int stopValue = 5;
+            DataService ds = new DataService();
+
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+
+
+
+            Console.WriteLine("Файл: " + res);
+
+
+            Console.WriteLine(" Создан!");
+            Console.ReadKey();
+        }
+    }
+}
