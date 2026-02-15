0a11fe2d45b2f229b637fb8c5510be3934dc7077
gnide <gnide@DESKTOP-FA1JVTU>
2026-02-15 19:48:24 +0500

    Task4

---
+++ Tyuiu.GnidenkoPA.Sprint5.Task4.V10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Tyuiu.GnidenkoPA.Sprint5.Task4.V10.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint5.Task4.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет значение по формуле и возвращает  *");
+            Console.WriteLine("* полученный результат                                                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\Users\avv\Desktop\DataSprint5\InPutDataFileTask4V10.txt";
+            Console.WriteLine($"Данные находятся в файле: {path}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
