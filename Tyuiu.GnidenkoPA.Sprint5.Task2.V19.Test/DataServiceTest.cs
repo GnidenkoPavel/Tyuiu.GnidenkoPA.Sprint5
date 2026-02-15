403c7965f5a8858cd0113deec75675f903e77ff5
gnide <gnide@DESKTOP-FA1JVTU>
2026-02-15 19:13:13 +0500

    Task2

---
+++ Tyuiu.GnidenkoPA.Sprint5.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.GnidenkoPA.Sprint5.Task2.V19.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint5.Task2.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestSaveToFileTextData()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix =
+            {
+                { 9, 2, 5 },
+                { 8, 8, 2 },
+                { 7, 4, 8 }
+            };
+
+            string path = ds.SaveToFileTextData(matrix);
+
+            Assert.IsTrue(File.Exists(path));
+
+            string[] lines = File.ReadAllLines(path);
+            Assert.AreEqual("0;2;0", lines[0]);
+            Assert.AreEqual("8;8;2", lines[1]);
+            Assert.AreEqual("0;4;8", lines[2]);
+        }
+    }
+}
---
+++ Tyuiu.GnidenkoPA.Sprint5.Task2.V19/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Tyuiu.GnidenkoPA.Sprint5.Task2.V19.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint5.Task2.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Дан целочисленный массив 3x3. Заменить нечётные элементы на 0           *");
+            Console.WriteLine("* и сохранить результат в файл OutPutFileTask2.csv                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine();
+
+            int[,] matrix = new int[3, 3];
+            Console.WriteLine("Введите элементы матрицы 3x3:");
+
+            for (int i = 0; i < 3; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                for (int j = 0; j < 3; j++)
+                {
+                    matrix[i, j] = int.Parse(input[j]);
+                }
+            }
+
+            DataService ds = new DataService();
+            string path = ds.SaveToFileTextData(matrix);
+
+            Console.WriteLine();
+            Console.WriteLine($"Результат сохранён в файл: {path}");
+        }
+    }
+}
