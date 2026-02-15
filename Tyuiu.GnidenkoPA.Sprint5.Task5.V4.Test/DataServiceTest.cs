4975f42ae9b8ff1676a3976f451afa3e39e71bc9
gnide <gnide@DESKTOP-FA1JVTU>
2026-02-15 20:04:40 +0500

    Task5

---
+++ Tyuiu.GnidenkoPA.Sprint5.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.GnidenkoPA.Sprint5.Task5.V4.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint5.Task5.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.GnidenkoPA.Sprint5.Task5.V4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Tyuiu.GnidenkoPA.Sprint5.Task5.V4.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint5.Task5.V4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            DataService ds = new DataService();
+            string path = @"C:\Users\User\source\repos\Tyuiu.TodikovDE.Sprint5\DataSprint5\InPutDataFileTask5V4.txt";
+            Console.WriteLine($"Данные находятся в файле: {path}");
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine($"Сумма чётных элементов файла = {res}");
+            Console.ReadKey();
+        }
+    }
+}
