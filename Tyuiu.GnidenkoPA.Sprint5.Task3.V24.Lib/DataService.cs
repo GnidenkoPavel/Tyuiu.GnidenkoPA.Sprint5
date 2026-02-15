198203027c4b180c479ae0983541a8bdfbb31af1
gnide <gnide@DESKTOP-FA1JVTU>
2026-02-15 19:31:13 +0500

    Task3

---
+++ Tyuiu.GnidenkoPA.Sprint5.Task3.V24.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.GnidenkoPA.Sprint5.Task3.V24.Lib
+{
+    public class DataService : ISprint5Task3V24
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Path.GetTempPath()}OutPutFileTask3.bin";
+            double y = Math.Round(6.1 * Math.Pow(x, 3) + 0.23 * Math.Pow(x, 2) + 1.04 * x, 3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.GnidenkoPA.Sprint5.Task3.V24/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Tyuiu.GnidenkoPA.Sprint5.Task3.V24.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint5.Task3.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            DataService ds = new DataService();
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                  *");
+            Console.WriteLine("*****************************************************************************************************");
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                        *");
+            Console.WriteLine("*****************************************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл:" + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
